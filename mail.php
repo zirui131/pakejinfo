a931e61514f4c5ce69b029c5f9d8cc5224d00e2d
Zi Rui 131 <119091582+zirui131@users.noreply.github.com>
2023-10-07 11:41:37 +0800

    mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,17 @@
 <?php
 //get data from form  
 $pakejinfo = $_POST['pakejinfo'];
+
 $to = "ngzirui131@gmail.com";
 
 $subject = "Mail From Pakej Info CLHS Travelling service";
-$txt ="Pakej info = ". $pakejinfo . ";
+$txt ="Pakej info = ". $pakejinfo . ;
 $headers = "From: ngzirui131@gmail.com" . "\r\n" .
 "CC: somebodyelse@example.com";
+
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
+    echo "Terima kasih!";
 }
 //redirect
-echo "Terima kasih!";
 ?>
